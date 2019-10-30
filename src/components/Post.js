09eda5570257c2a3d641d00c4119ab0d6f70bb66
farhan <patwary.farhan87@gmail.com>
2019-10-30 19:40:28 +0000

    added user link to post

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,21 +5,19 @@ import Accordion from 'react-bootstrap/Accordion'
 
 const Post = (props) => {
     var imgurl = `http://localhost:8000/posts/${props.id}/image`
-    console.log(props)
     
     return (
         <Accordion>
             <Card>
                 <Accordion.Toggle as={Card.Header} variant="link" eventKey="1">
                     <Card.Title>{props.title}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted"><a>{props.name}</a></Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted"><a href="/">By: {props.name}</a></Card.Subtitle>
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="1">
                     <Card.Body>
                         <p>{props.description}</p>
                         <Image src={imgurl} alt="" fluid />
                     </Card.Body>
-                    
                 </Accordion.Collapse>
             </Card>
         </Accordion>
