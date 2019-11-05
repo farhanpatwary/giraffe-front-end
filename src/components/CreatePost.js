bd5d94ea013b0fe93449f8535c8c28cf71c3e3fa
farhan <patwary.farhan87@gmail.com>
2019-11-05 20:05:54 +0000

    createPost no longer available to unauthenticated users

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -3,6 +3,7 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import Cookies from 'universal-cookie';
+import { Redirect } from 'react-router-dom';
 
 const axios = require('axios');
 class CreatePost extends Component {
@@ -56,46 +57,61 @@ class CreatePost extends Component {
             if(e.response){
                 console.log(JSON.stringify(e));
             }
-        }).then(this.props.history.push('/'))
+        }).then((resp)=>{
+            console.log(resp)
+            this.props.history.push('/')
+        })
     }
     
     render() {
-      return (
-        <Container>
-            <br/>
-            <Form onSubmit={this.handleSubmit}>
-            <Form.Group controlId="title">
-                <Form.Label>Title</Form.Label>
-                <Form.Control type="text" placeholder="Insert title" onChange={this.handleChange}/>
-                
-            </Form.Group>
+        const cookies = new Cookies();
+        const authtoken = cookies.get('token');
+        if(authtoken === undefined){
+            alert('Please sign in')
+            return <Redirect to='/' />
+        }
+        else {
+            let upload_text = this.state.image === null ? 'Choose file' : this.state.image.name
+            return (
+                <Container>
+                    <br/>
+                    <Form onSubmit={this.handleSubmit}>
+                    <Form.Group controlId="title">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control type="text" placeholder="Insert title" onChange={this.handleChange}/>
+                        
+                    </Form.Group>
 
-            <Form.Group controlId="description">
-                <Form.Label>Description</Form.Label>
-                <Form.Control type="text" placeholder="Insert Description" onChange={this.handleChange}/>
-            </Form.Group>
-            <Form.Group controlId="image">
-                <div className="input-group">
-                <div className="input-group-prepend">
-                    <span className="input-group-text" id="image">
-                    Upload
-                    </span>
-                </div>
-                <div className="custom-file">
-                    <input type="file" className="custom-file-input" id="image" aria-describedby="image" onChange={this.handleChange}/>
-                    <label className="custom-file-label" htmlFor="image">
-                    Choose file
-                    </label>
-                </div>
-                </div>
-            </Form.Group>
-            
-            <Button variant="outline-primary" type="submit" block>
-                Post
-            </Button>
-            </Form>
-        </Container>
-      )
+                    <Form.Group controlId="description">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control type="text" as="textarea" placeholder="Insert Description" onChange={this.handleChange}/>
+                    </Form.Group>
+                    <Form.Group controlId="image">
+                        <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="image">
+                            Upload
+                            </span>
+                        </div>
+                        <div className="custom-file">
+                            <input type="file" className="custom-file-input" id="image" aria-describedby="image" onChange={this.handleChange}/>
+                            <label className="custom-file-label" htmlFor="image">
+                            {upload_text}
+                            </label>
+                        </div>
+                        </div>
+                        <Form.Text className="text-muted">
+                        Allowed Uploads: png, jpg, jpeg | Max File Upload Size: 1MB
+                        </Form.Text>
+                    </Form.Group>
+                    
+                    <Button variant="outline-primary" type="submit" block>
+                        Post
+                    </Button>
+                    </Form>
+                </Container>
+            )
+        } 
     }
   }
 
