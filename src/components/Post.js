5748f20a9f8c5ccf71467e7a739035c54ee32103
farhan <patwary.farhan87@gmail.com>
2019-11-08 02:00:02 +0000

    added user page link on post

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,13 +7,16 @@ const Post = (props) => {
     var imgurl = `http://localhost:8000/posts/${props.id}/image`
     const time = props.dateCreated.slice(11,16)
     const date = props.dateCreated.slice(0,10)
-
+    const user_page_path = `/users/${props.owner_id}`
+    const link_style = {
+        'color':'black'
+    }
     return (
         <Accordion>
             <Card>
                 <Accordion.Toggle as={Card.Header} variant="link" eventKey="1">
                     <Card.Title>{props.title}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted"><a href="/">By: {props.name}</a></Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted"><a href={user_page_path} style={link_style}>By: {props.owner_name}</a></Card.Subtitle>
                     <Card.Subtitle className="mb-2 text-muted">Created at: {time} on {date}</Card.Subtitle>    
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="1">
