b0ff335eb34b889dba30717deb3f225aa2b6e54d
farhan <patwary.farhan87@gmail.com>
2019-11-08 02:02:40 +0000

    added redirect and made sign up sign new user in

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -3,6 +3,7 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import Cookies from 'universal-cookie';
+import { Redirect } from 'react-router-dom';
 export default class SignUpForm extends Component {
 
     constructor(props) {
@@ -24,7 +25,7 @@ export default class SignUpForm extends Component {
           [id]: value
         });
     }
-    async handleSubmit(event) {
+    handleSubmit(event) {
         event.preventDefault();
         if(this.state.email === '' || this.state.password === ''){
             alert('Please enter email and password')
@@ -35,43 +36,62 @@ export default class SignUpForm extends Component {
             email: this.state.email,
             password: this.state.password
         }
-        let data = await fetch('http://localhost:8000/users', {
+        fetch('http://localhost:8000/users', {
             method: 'POST',
             body: JSON.stringify(formdata),
             headers: {
                 'Content-Type':'application/json',
             }    
         })
-        const jsondata = await data.json()
-        const token = jsondata.token
-        const cookies = new Cookies();
-        cookies.set('token', token);
+        .then((data)=>{
+            if(data.ok === false){
+                return alert('User already exists. Please sign in or create a new user.')
+            }
+            data.json()
+            .then((jsondata) => {
+                const token = jsondata.token
+                const cookies = new Cookies()
+                cookies.set('token', token)
+                cookies.set('user', jsondata.user.name)
+                this.props.signIn()
+            })
+        })
+        
+        
     }
 
     render() {
-        return (
-            <Container>
-                <br/>
-                <h3>Sign Up to Giraffe</h3>
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group controlId="name">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control type="name" placeholder="Name" onChange={this.handleChange} />
-                    </Form.Group>
-                    <Form.Group controlId="email">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" onChange={this.handleChange} />
-                    </Form.Group>
-                    <Form.Group controlId="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" onChange={this.handleChange}/>
-                    </Form.Group>
-                    <p>Already Registered? <a href="/login">Sign In here.</a></p>
-                    <Button variant="outline-primary" type="submit">
-                        Sign Up
-                    </Button>
-                </Form>
-            </Container>
-        )
+        if(this.props.signed_in === true){
+            return <Redirect to='/' />
+        }
+        else {
+            return (
+                <Container>
+                    <br/>
+                    <h3>Sign Up to Giraffe</h3>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group controlId="name">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="name" placeholder="Name" onChange={this.handleChange} />
+                        </Form.Group>
+                        <Form.Group controlId="email">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" onChange={this.handleChange} />
+                        </Form.Group>
+                        <Form.Group controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" onChange={this.handleChange}/>
+                            <Form.Text className="text-muted">
+                                Password must be at least 7 characters.
+                            </Form.Text>
+                        </Form.Group>
+                        <p>Already Registered? <a href="/login">Sign In here.</a></p>
+                        <Button variant="outline-primary" type="submit">
+                            Sign Up
+                        </Button>
+                    </Form>
+                </Container>
+            )    
+        }
     }
 }
